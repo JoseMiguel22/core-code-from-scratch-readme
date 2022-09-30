d8a639659eef01fc712f087dd182a6e9b60a1973
Jose Romero <josemiguelr543@gmail.com>
2022-09-30 03:46:16 -0400

    First commit Generics

---
+++ Generics/Node.ts
@@ -0,0 +1,12 @@
+export default class Node<T> {
+  public next: Node<T> | null = null;
+  private data: T;
+
+  get value(): T {
+    return this.data;
+  }
+
+  constructor(data: T) {
+    this.data = data;
+  }
+}
