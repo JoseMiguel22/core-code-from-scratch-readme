5db71cc1a02417e9ec80ecb85434216e73bb069b
Jose Romero <josemiguelr543@gmail.com>
2022-10-12 01:51:03 -0400

    first commit

---
+++ migjo-node/main.js
@@ -0,0 +1,12 @@
+const { sum, subs} = require("./operations")
+
+
+const number0 = sum(30, 20);
+const number1 = sum(50, 20);
+const number2 = subs(30, 20);
+const number3 = subs(50, 20);
+
+console.log(number0)
+console.log(number1)
+console.log(number2)
+console.log(number3)
