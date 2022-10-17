073d42ec70bf62d18ef8f5c97438c99a24e89524
Jose Romero <josemiguelr543@gmail.com>
2022-10-17 00:46:02 -0400

    first commit

---
+++ TicTacToe/models/players.ts
@@ -0,0 +1,10 @@
+export default class Player {
+    get name(){
+        return this.nickname;
+    }
+
+    set name(name: string){
+        this.nickname = name;
+    }
+    constructor(private nickname: string){}
+}
